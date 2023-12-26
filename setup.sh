9b0152862db4f9351b441a0763776e3fc6afbaf7
Elyviere <elyviere@gmail.com>
2023-12-26 21:27:31 +0100

    Changed from git https to ssl

--- setup.sh
+++ setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent?platform=linux
+# ssh-keygen -t ed25519 -C "elyviere@gmail.com"
+# Enter a file in which to save the key: ~/.ssh/id_ed25519
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
+# cat ~/.ssh/id_ed25519.pub 
+# Copy contents of this to github: https://github.com/settings/ssh/new
+
 # mkdir ~/.config
-# git clone https://github.com/Elyviere/configfiles.git ~/.config/configfiles
+# git clone git@github.com:Elyviere/configfiles.git ~/.config/configfiles
 # chmod u+x ~/.config/configfiles/setup.sh
 # sh ~/.config/configfiles/setup.sh
 
