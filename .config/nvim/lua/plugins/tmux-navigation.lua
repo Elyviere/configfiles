ed8275175066a046346644a29a7762a6b69cae15
Elyviere <elyviere@gmail.com>
2024-04-23 19:59:25 +0200

    Fix nvim-tmux-navigation from Nvim to Tmux

--- .config/nvim/lua/plugins/tmux-navigation.lua
+++ .config/nvim/lua/plugins/tmux-navigation.lua
@@ -1,14 +1,6 @@
 return {
   "alexghergh/nvim-tmux-navigation",
   config = function()
-    require("nvim-tmux-navigation").setup({
-      keybindings = {
-        left = "<C-h>",
-        right = "<C-L>",
-        up = "<C-k>",
-        down = "<C-j>",
-        last_active = "<C-\\>",
-      },
-    })
+    require("nvim-tmux-navigation").setup({})
   end,
 }
