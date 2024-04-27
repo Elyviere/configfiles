ca3773b9873292247ac9f591309f39e4f73efb95
Elyviere <elyviere@gmail.com>
2024-04-27 09:14:12 +0200

    Move tmux-navigation keymappings to the tmux-navigation.lua file

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,10 +4,3 @@
 
 vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set("v", "<C-c>", "<Esc>", { desc = "Exit visual mode" })
-
--- Tmux Navigation (tmux-navigation.lua)
-vim.keymap.set("n", "<C-h>", require("nvim-tmux-navigation").NvimTmuxNavigateLeft)
-vim.keymap.set("n", "<C-j>", require("nvim-tmux-navigation").NvimTmuxNavigateDown)
-vim.keymap.set("n", "<C-k>", require("nvim-tmux-navigation").NvimTmuxNavigateUp)
-vim.keymap.set("n", "<C-l>", require("nvim-tmux-navigation").NvimTmuxNavigateRight)
-vim.keymap.set("n", "<C-\\>", require("nvim-tmux-navigation").NvimTmuxNavigateLastActive)
--- .config/nvim/lua/plugins/tmux-navigation.lua
+++ .config/nvim/lua/plugins/tmux-navigation.lua
@@ -1,6 +1,12 @@
 return {
   "alexghergh/nvim-tmux-navigation",
-  config = function()
-    require("nvim-tmux-navigation").setup({})
+  keys = function()
+    return {
+      { "<C-h>", require("nvim-tmux-navigation").NvimTmuxNavigateLeft },
+      { "<C-j>", require("nvim-tmux-navigation").NvimTmuxNavigateDown },
+      { "<C-k>", require("nvim-tmux-navigation").NvimTmuxNavigateUp },
+      { "<C-l>", require("nvim-tmux-navigation").NvimTmuxNavigateRight },
+      { "<C-\\>", require("nvim-tmux-navigation").NvimTmuxNavigateLastActive },
+    }
   end,
 }
